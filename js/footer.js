e1f16e061136ff516d4ac9828c8391aa9519c645
tsumugi1012 <k1111t1012@icloud.com>
2025-07-21 13:34:20 +0900

    Delete js directory

--- js/footer.js
+++
@@ -1,6 +0,0 @@
-fetch("parts/footer.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("footer-placeholder").innerHTML = data;
-  })
-  .catch(error => console.error("フッターの読み込みに失敗しました:", error));
